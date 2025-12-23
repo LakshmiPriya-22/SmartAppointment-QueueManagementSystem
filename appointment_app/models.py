2f84c4243f8d072b5d7e2c9a07b99453a84ffa36
LakshmiPriya-22 <lakshmipriya6177@gmail.com>
2025-12-23 13:26:17 +0530

    Add Appointment model with status and timestamps

--- appointment_app/models.py
+++ appointment_app/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.utils import timezone
-
 
 class Appointment(models.Model):
     STATUS_CHOICES = [
@@ -13,20 +11,15 @@ class Appointment(models.Model):
     service = models.CharField(max_length=100)
 
     token_number = models.PositiveIntegerField(unique=True)
+
     status = models.CharField(
         max_length=10,
         choices=STATUS_CHOICES,
         default='pending'
     )
 
-    booked_at = models.DateTimeField(default=timezone.now)
+    booked_at = models.DateTimeField(auto_now_add=True)
     called_at = models.DateTimeField(null=True, blank=True)
-
-    def save(self, *args, **kwargs):
-        if not self.token_number:
-            last = Appointment.objects.order_by('-token_number').first()
-            self.token_number = 1 if not last else last.token_number + 1
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Token {self.token_number} - {self.name}"
