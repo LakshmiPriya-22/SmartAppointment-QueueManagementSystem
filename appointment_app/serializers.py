5c26582d73452cd4d4e438d62c01079b989b45b1
LakshmiPriya-22 <lakshmipriya6177@gmail.com>
2025-12-24 11:56:01 +0530

    Implement auto token number generation

--- appointment_app/serializers.py
+++ appointment_app/serializers.py
@@ -5,4 +5,3 @@ class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
         fields = '__all__'
-        read_only_fields = ['token_number', 'status', 'booked_at', 'called_at']
